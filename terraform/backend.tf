45460ccb0fd7b0e0c8966c2b313ae050e611fe34
PritamDivekar <divekarpritam0@gmail.com>
2026-01-30 18:57:03 +0530

    Add remote Terraform backend

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "airport-intelligence-terraform-state"
+    key            = "infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
